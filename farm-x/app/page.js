20ab8005d3a1ec89d903b421b78988afb2934fb5
lowell-monis <monislow@msu.edu>
2024-09-28 22:04:29 -0400

    created color pallete

--- farm-x/app/page.js
+++ farm-x/app/page.js
@@ -1,17 +1,35 @@
 "use client"
 import { useState, useEffect } from "react"
-import Image from "next/image"
 import { Box, BottomNavigation, BottomNavigationAction, Typography } from "@mui/material"
-import { HomeIcon, AccountCircleIcon, AddIcon, RestoreIcon, FavoriteIcon, LocationOnIcon } from "@mui/icons-material"
+import RestoreIcon from "@mui/icons-material/Restore";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import { palette } from '@mui/system';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { lime, purple } from '@mui/material/colors';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#18453b',
+      light: '#2c7e6c',
+      dark: '#040c0a',
+      contrastText: '#fff',
+    },
+  },
+});
+
 export default function Home() {
   const [value, setValue] = useState(0);
   return (
 
-    <Box>
+    <ThemeProvider theme={theme}>
+      <Box>
       <Typography variant="h1">FarmX</Typography>
       <BottomNavigation
         showLabels
         value={value}
+        sx={{ width: '100%', position: 'absolute', bottom: 0, color: 'primary' }}
         onChange={(event, newValue) => {
           setValue(newValue);
         }}
@@ -21,5 +39,6 @@ export default function Home() {
         <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
       </BottomNavigation>
     </Box>
+    </ThemeProvider>
   )
 }
