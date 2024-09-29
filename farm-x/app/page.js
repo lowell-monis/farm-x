311485a2c91e4b8c0b383b79a61adc00c9e60bbb
lowell-monis <monislow@msu.edu>
2024-09-28 21:40:44 -0400

    downloaded mui icon modules

--- farm-x/app/page.js
+++ farm-x/app/page.js
@@ -2,10 +2,24 @@
 import { useState, useEffect } from "react"
 import Image from "next/image"
 import { Box, BottomNavigation, BottomNavigationAction, Typography } from "@mui/material"
+import { HomeIcon, AccountCircleIcon, AddIcon, RestoreIcon, FavoriteIcon, LocationOnIcon } from "@mui/icons-material"
 export default function Home() {
+  const [value, setValue] = useState(0);
   return (
+
     <Box>
-      <Typography>FarmX</Typography>
+      <Typography variant="h1">FarmX</Typography>
+      <BottomNavigation
+        showLabels
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+      >
+        <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
+        <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
+        <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+      </BottomNavigation>
     </Box>
   )
 }
