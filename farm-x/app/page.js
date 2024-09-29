1e022ee088fdcd984e4c2616ad50b5ba67f629ec
lowell-monis <monislow@msu.edu>
2024-09-28 21:27:08 -0400

    created next app, imported mui, started base setup

---
+++ farm-x/app/page.js
@@ -0,0 +1,11 @@
+"use client"
+import { useState, useEffect } from "react"
+import Image from "next/image"
+import { Box, BottomNavigation, BottomNavigationAction, Typography } from "@mui/material"
+export default function Home() {
+  return (
+    <Box>
+      <Typography>FarmX</Typography>
+    </Box>
+  )
+}
